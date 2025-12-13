0a68bde45fa0787a964f6c3ff1ef7e136a6ae0cd
Pinkie2070 <pinkie2070@gmail.com>
2025-12-13 19:35:21 +0100

    feat(config): Update configuration for locale, job creator, and command settings; add Discord webhook URLs

--- config.lua
+++ config.lua
@@ -1,14 +1,12 @@
 --[[
-    https://github.com/kxiox/ks_bossmenu
-
+    [https://github.com/kxiox/ks_bossmenu](https://github.com/kxiox/ks_bossmenu)
     This file is licensed under GPL-3.0 or higher <https://www.gnu.org/licenses/gpl-3.0.en.html>
-
     Copyright © 2025 Kxiox <https://github.com/kxiox>
 ]]
 
 Config = {}
 
-Config.Locale = 'en'
+Config.Locale = 'de'
 
 Config.Currency = '$'
 
@@ -16,11 +14,11 @@ Config.Timestamp = 'us'
 
 Config.UnemployedJobName = 'unemployed'
 
-Config.UseTarget = false -- Use ox_target for the boss menu (resource name: ox_target)
+Config.UseTarget = false        -- Use ox_target for the boss menu (resource name: ox_target)
 
-Config.UseJobsCreator = false -- only for jaksam's jobcreator (resouerce name: jobs_creator)
+Config.UseJobsCreator = true    -- only for jaksam's jobcreator (resource name: jobs_creator)
 
-Config.EnableCommand = false -- Enable the command to open the boss menu | NEW
+Config.EnableCommand = true     -- Enable the command to open the boss menu | NEW
 Config.CommandName = 'bossmenu' -- The name of the command | NEW
 
 Config.Jobs = {
@@ -28,24 +26,24 @@ Config.Jobs = {
         society = nil,
         logo = 'police.png',
         color = 'blue',
-        grades = { 4 },
+        grades = { 15, 14, 13, 12, 11 },
         marker = {
-            type = 1,
-            coords = vector3(441.0, -981.0, 30.0),
-            scale = vector3(1.5, 1.5, 1.5),
+            type = 21,
+            coords = vector3(-1108.64, -836.68, 34.28),
+            scale = vector3(0.3, 0.3, 0.2),
             color = { r = 0, g = 0, b = 255, a = 200 },
         },
-    }
+    },
 }
 
-Config.Menus = { -- NEW (de-/activate menu features)
+Config.Menus = { -- (de-/activate menu features)
     employees = {
         enabled = true
     },
 
     salaries = {
         enabled = true,
-        maximum = nil, -- NEW maximum salary change (nil = no limit; integer value)
+        maximum = 5000, -- maximum salary change (nil = no limit; integer value)
     },
 
     account = {
@@ -54,6 +52,6 @@ Config.Menus = { -- NEW (de-/activate menu features)
 
     bonus = {
         enabled = true,
-        maximum = nil, -- NEW maximum bonus amount (nil = no limit; integer value)
+        maximum = nil, -- maximum bonus amount (nil = no limit; integer value)
     }
 }
--- webhooks.lua
+++ webhooks.lua
@@ -7,15 +7,24 @@
 ]]
 
 Config.Webhooks = {
-    hire = "YOUR_DISCORD_WEBHOOK_URL",
-    fire = "YOUR_DISCORD_WEBHOOK_URL",
-    promote = "YOUR_DISCORD_WEBHOOK_URL",
-    demote = "YOUR_DISCORD_WEBHOOK_URL",
-    deposit = "YOUR_DISCORD_WEBHOOK_URL",
-    withdraw = "YOUR_DISCORD_WEBHOOK_URL",
-    salary = "YOUR_DISCORD_WEBHOOK_URL",
-    bonus = "YOUR_DISCORD_WEBHOOK_URL",
-    general = "YOUR_DISCORD_WEBHOOK_URL"
+    hire =
+    "https://discord.com/api/webhooks/1402771440121675846/vMPR7RnF6pFJ_aoY9dySLCuYYebD_bg5X3WACSttK7PGh2wQmRybgKeICChdYm62d8DH",
+    fire =
+    "https://discord.com/api/webhooks/1402771440121675846/vMPR7RnF6pFJ_aoY9dySLCuYYebD_bg5X3WACSttK7PGh2wQmRybgKeICChdYm62d8DH",
+    promote =
+    "https://discord.com/api/webhooks/1402771440121675846/vMPR7RnF6pFJ_aoY9dySLCuYYebD_bg5X3WACSttK7PGh2wQmRybgKeICChdYm62d8DH",
+    demote =
+    "https://discord.com/api/webhooks/1402771440121675846/vMPR7RnF6pFJ_aoY9dySLCuYYebD_bg5X3WACSttK7PGh2wQmRybgKeICChdYm62d8DH",
+    deposit =
+    "https://discord.com/api/webhooks/1402771440121675846/vMPR7RnF6pFJ_aoY9dySLCuYYebD_bg5X3WACSttK7PGh2wQmRybgKeICChdYm62d8DH",
+    withdraw =
+    "https://discord.com/api/webhooks/1402771440121675846/vMPR7RnF6pFJ_aoY9dySLCuYYebD_bg5X3WACSttK7PGh2wQmRybgKeICChdYm62d8DH",
+    salary =
+    "https://discord.com/api/webhooks/1402771440121675846/vMPR7RnF6pFJ_aoY9dySLCuYYebD_bg5X3WACSttK7PGh2wQmRybgKeICChdYm62d8DH",
+    bonus =
+    "https://discord.com/api/webhooks/1402771440121675846/vMPR7RnF6pFJ_aoY9dySLCuYYebD_bg5X3WACSttK7PGh2wQmRybgKeICChdYm62d8DH",
+    general =
+    "https://discord.com/api/webhooks/1402771440121675846/vMPR7RnF6pFJ_aoY9dySLCuYYebD_bg5X3WACSttK7PGh2wQmRybgKeICChdYm62d8DH"
 }
 
 Config.JobWebhooks = {
